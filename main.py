76f3cae314d33120c5b196878c3f0a976429985d
Ahmed Hajeyah <hajeyah@gmail.com>
2022-07-05 19:17:25 +0300

    Soled

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # printer(elements)
 # - Accepts a list
 # - Prints every element of the list
+
+from readline import append_history_file
+from xml.dom.minidom import Element
+
+
+# elements = [1, 2, 3, 4, 5]
+
 def printer(elements):
     # Your code here
-    ...
+    for element in elements:
+        print(element)
+    
+# printer(elements)
 
 
 # to_celsius(temperatures)
@@ -13,19 +23,41 @@ def printer(elements):
 #   in degrees Celsius
 # The conversion is:
 #   C = (F - 32) * (5/9)
+
+# temperatures_list = [100,120,99]
+
 def to_celsius(temperatures):
     # Your code here
-    ...
+    ctemp=[]
+    for temp in temperatures:
+        ctemp.append((temp-32)*5/9)
+    print(ctemp)
+    return ctemp
 
+# to_celsius(temperatures_list)
 
 # hottest_days(temperatures, threshold)
 # - Accepts a list of temperatures
 # - Accepts a threshold temperature
 # - Returns a list of temperatures
 #   that exceed the threshold
+
+
+# temperatures_list = [102,120,99,103,120]
+# threshold_temp = 100
+
 def hottest_days(temperatures, threshold):
     # Your code here
-    ...
+    temp=[]
+    temp_above_threshold = []
+    for temp in temperatures:
+        if temp >= threshold:
+            print(temp)
+            temp_above_threshold.append(temp)
+    return temp_above_threshold
+
+#hottest_days(temperatures_list, threshold_temp)
+     
 
 
 # log_hottest_days(temperatures, threshhold)
@@ -37,6 +69,16 @@ def hottest_days(temperatures, threshold):
 #   threshold IN DEGREES CELSIUS
 # hint: you can combine
 #       all previous functions
+
+temperatures_list = [102,120,99,103,120]
+threshold_temp = 100
+
 def print_hottest_days(temperatures, threshhold):
     # Your code here
-    ...
+    for tempa in temperatures:
+        print(tempa)
+    to_celsius(temperatures)
+    hottest_days(temperatures, threshhold)
+
+print_hottest_days(temperatures_list, threshold_temp)
+
